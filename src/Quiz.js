765c555249c44132aa374013a4f2990fcdb170d3
imzikra <ramzaa.zikra@gmail.com>
2023-04-07 20:33:46 +0530

    1 file modified, 1 added

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import QuizQuestion from './QuizQuestion.js';
+import QuizEnd from './QuizEnd.js';
+
 let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
@@ -7,12 +9,29 @@ class Quiz extends Component {
     super(props);
     this.state = { quiz_position: 1 };
   }
+  showNextQuestion() {
+    this.setState((state) => {
+      return { quiz_position: state.quiz_position + 1 };
+    });
+  }
   render() {
+    const isQuizEnd =
+      this.state.quiz_position - 1 === quizData.quiz_questions.length;
     return (
       <div>
-        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+        {isQuizEnd ? (
+          <QuizEnd />
+        ) : (
+          <QuizQuestion
+            quiz_question={
+              quizData.quiz_questions[this.state.quiz_position - 1]
+            }
+            showNextQuestionHandler={this.showNextQuestion.bind(this)}
+          />
+        )}
       </div>
     );
   }
 }
+
 export default Quiz;
