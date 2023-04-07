a5e472176782d04fb18d225e8bcebfd0e3651913
imzikra <ramzaa.zikra@gmail.com>
2023-04-07 19:34:57 +0530

    2 files modified

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
+let quizData = require('./quiz_data.json');
 
-let quizData = require('./quiz_data.json')
-
-class Quiz extends Component{
-render(){
-  return 
-  <div>
-    Quiz
-    </div>;
-}
+class Quiz extends Component {
+  constructor(props){
+    super(props)
+    this.state { quiz_position: 1}
+  }
+  render() {
+    return <div>
+      <div className="QuizQuestion">{quizDat.quiz_questions[0].instruction_text}
+        </div>
+        </div>;
+  }
 }
 export default Quiz;
